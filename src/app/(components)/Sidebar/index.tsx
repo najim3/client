32540ca1b79b7ba9d9688bce6e569780f679e677
najim3 <engr.najim3@gmail.com>
2024-08-10 17:14:05 +0600

    2

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,27 @@
+import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">NUSTOCK</h1>
+
+        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100">
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex-grow mt-8">{/* links here */}</div>
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 NUSTOCK</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
